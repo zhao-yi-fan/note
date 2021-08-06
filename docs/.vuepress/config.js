d35d823ce631df443144cf00f08a92c95447ccc1
zhaoyifan <993445491@qq.com>
2021-08-07 01:47:55 +0800

    'init'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around',
-  base: '/note/'
+  title: "zhaoyifan Docs",
+  description: 'zhaoyifan Docs',
+  base: '/note/',
+  markdown: {
+    lineNumbers: true, // 是否在每个代码块的左侧显示行号。
+  },
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+    ],
+    sidebar: 'auto'
+  }
 }
