f82466c2a4950cb7c5125b7d7afc0c8aeae5a516
zhaoyifan <zhaoyifan@fehorizon.com>
2021-08-12 19:58:50 +0800

    'feat:子组增加'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,132 +19,189 @@ module.exports = {
     ],
     sidebar: [
       {
-        title: 'express',   // 必要的
-        path: '/express/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        title: 'express',
+        path: '/express/', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
         collapsable: false, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-        sidebarDepth: 1,    // 可选的, 默认值是 1
+        sidebarDepth: 1, // 可选的, 默认值是 1
         children: [],
       },
       {
-        title: 'flutter',   // 必要的
-        path: '/flutter/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        collapsable: false, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-        sidebarDepth: 1,    // 可选的, 默认值是 1
+        title: 'flutter',
+        path: '/flutter/',
+        collapsable: false,
+        sidebarDepth: 1,
         children: []
       },
       {
-        title: 'javascript汇总',   // 必要的
-        path: '/javascript/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        collapsable: false, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-        sidebarDepth: 1,    // 可选的, 默认值是 1
+        title: 'javascript汇总',
+        path: '/javascript/',
+        collapsable: false,
+        sidebarDepth: 1,
         children: []
       },
       {
-        title: 'Koa',   // 必要的
-        path: '/Koa/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        collapsable: false, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-        sidebarDepth: 1,    // 可选的, 默认值是 1
+        title: 'Koa',
+        path: '/Koa/',
+        collapsable: false,
+        sidebarDepth: 1,
         children: []
       },
       {
-        title: 'mongoDB',   // 必要的
-        path: '/mongoDB/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        collapsable: false, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-        sidebarDepth: 1,    // 可选的, 默认值是 1
+        title: 'mongoDB',
+        path: '/mongoDB/',
+        collapsable: false,
+        sidebarDepth: 1,
         children: []
       },
       {
-        title: 'nginx',   // 必要的
-        path: '/nginx/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        collapsable: false, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-        sidebarDepth: 1,    // 可选的, 默认值是 1
+        title: 'nginx',
+        path: '/nginx/',
+        collapsable: false,
+        sidebarDepth: 1,
         children: []
       },
       {
-        title: 'TypeScript',   // 必要的
-        path: '/TypeScript/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        collapsable: false, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-        sidebarDepth: 1,    // 可选的, 默认值是 1
+        title: 'TypeScript',
+        path: '/TypeScript/',
+        collapsable: false,
+        sidebarDepth: 1,
         children: []
       },
       {
-        title: 'vnm商城',   // 必要的
-        path: '/vnm-shop/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        collapsable: false, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-        sidebarDepth: 1,    // 可选的, 默认值是 1
+        title: 'vnm商城',
+        path: '/vnm-shop/',
+        collapsable: false,
+        sidebarDepth: 1,
         children: []
       },
       {
-        title: 'vue',   // 必要的
-        path: '/vue/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        collapsable: false, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-        sidebarDepth: 1,    // 可选的, 默认值是 1
+        title: 'vue',
+        path: '/vue/',
+        collapsable: false,
+        sidebarDepth: 1,
         children: []
       },
       {
-        title: 'web前端',   // 必要的
-        path: '',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        collapsable: true, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-        sidebarDepth: 3,    // 可选的, 默认值是 1
+        title: 'web前端',
+        path: '',
+        collapsable: true,
+        sidebarDepth: 3,
         children: [
           {
-            title: 'css',   // 必要的
-            path: '/web/css/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-            collapsable: true, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-            sidebarDepth: 1,    // 可选的, 默认值是 1
+            title: 'css',
+            path: '/web/css/',
+            collapsable: true,
+            sidebarDepth: 1,
             children: []
           },
           {
-            title: 'html',   // 必要的
-            path: '/web/html/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-            collapsable: true, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-            sidebarDepth: 1,    // 可选的, 默认值是 1
+            title: 'html',
+            path: '/web/html/',
+            collapsable: true,
+            sidebarDepth: 1,
             children: []
           },
           {
-            title: 'javascript',   // 必要的
-            path: '/web/javascript/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-            collapsable: true, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-            sidebarDepth: 1,    // 可选的, 默认值是 1
+            title: 'javascript',
+            path: '/web/javascript/',
+            collapsable: true,
+            sidebarDepth: 1,
             children: []
           },
           {
-            title: 'jQuery',   // 必要的
-            path: '/web/jQuery/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-            collapsable: true, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-            sidebarDepth: 1,    // 可选的, 默认值是 1
+            title: 'jQuery',
+            path: '/web/jQuery/',
+            collapsable: true,
+            sidebarDepth: 1,
             children: []
           },
           {
-            title: 'nodejs',   // 必要的
-            path: '/web/node.js/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-            collapsable: true, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-            sidebarDepth: 1,    // 可选的, 默认值是 1
+            title: 'nodejs',
+            path: '/web/node.js/',
+            collapsable: true,
+            sidebarDepth: 1,
             children: []
           },
           {
-            title: 'php',   // 必要的
-            path: '/web/php/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-            collapsable: true, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-            sidebarDepth: 1,    // 可选的, 默认值是 1
+            title: 'php',
+            path: '/web/php/',
+            collapsable: true,
+            sidebarDepth: 1,
             children: []
           },
         ]
       },
-      // {
-      //   title: 'web前端架构',   // 必要的
-      //   path: '/zhufengweb/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-      //   collapsable: false, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-      //   sidebarDepth: 1,    // 可选的, 默认值是 1
-      //   children: [
-
-      //   ]
-      // },
       {
-        title: 'webpack',   // 必要的
-        path: '/webpack/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        collapsable: false, // 侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
-        sidebarDepth: 1,    // 可选的, 默认值是 1
+        title: '珠峰web前端架构',
+        path: '',
+        collapsable: true,
+        sidebarDepth: 1,
+        children: [
+          {
+            title: 'ajax',
+            path: '/zhufengweb/AJAX/',
+            collapsable: true,
+            sidebarDepth: 1,
+            children: []
+          },
+          {
+            title: 'axios',
+            path: '/zhufengweb/AXIOS/',
+            collapsable: true,
+            sidebarDepth: 1,
+            children: []
+          },
+          {
+            title: 'DOM_Box_Model-JQ',
+            collapsable: true,
+            sidebarDepth: 1,
+            children: [
+              {
+                title: 'DOM_Box_Model',
+                path: '/zhufengweb/DOM_Box_Model',
+                collapsable: true,
+                sidebarDepth: 1,
+                children: [
+                  
+                ]
+              },
+            ]
+          },
+        ]
+      },
+      {
+        title: 'webpack',
+        path: '/webpack/',
+        collapsable: false,
+        sidebarDepth: 1,
+        children: []
+      },
+      {
+        title: '七层网络协议',
+        path: '/network-protocol/',
+        collapsable: false,
+        sidebarDepth: 1,
+        children: []
+      },
+      {
+        title: '微信小程序',
+        path: '/wechat_mini/',
+        collapsable: false,
+        sidebarDepth: 1,
+        children: []
+      },
+      {
+        title: 'websocket',
+        path: '/websocket/',
+        collapsable: false,
+        sidebarDepth: 1,
+        children: []
+      },
+      {
+        title: 'git',
+        path: '/git/',
+        collapsable: false,
+        sidebarDepth: 1,
         children: []
       },
     ],
--- docs/README.md
+++ docs/README.md
@@ -7,10 +7,6 @@ actionLink: /express/
 features:
 - title: 简洁至上
   details: 111
-- title: Vue驱动
-  details: 222
-- title: 高性能
-  details: 333
 footer: MIT Licensed | Copyright © 2021-present zhaoyifan
 ---
 # Hello VuePress
