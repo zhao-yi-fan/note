d730d2c67b3b9f5d504fb1915775b94c11e2c3d7
zhaoyifan <zhaoyifan@fehorizon.com>
2021-08-16 20:19:47 +0800

    仓库链接、navbar增加

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,6 +13,8 @@ module.exports = {
     }
   },
   themeConfig: {
+    repo: 'https://github.com/zhao-yi-fan/note/',
+    // repoLabel: '', // 项目仓库的标签
     lastUpdated: '上次更新', // string | boolean
     navbar: [
       {
@@ -68,6 +70,44 @@ module.exports = {
             ]
           },
           {
+            text: '事件绑定、事件委托、发布订阅',
+            children: [
+              '/zhufengweb/event_binding-publish_subscribe-event-event_delegation/event_binding-publish_subscribe/README.md',
+              '/zhufengweb/event_binding-publish_subscribe-event-event_delegation/event-event_delegation/README.md',
+            ]
+          },
+          {
+            text: 'this指向、正则、原型链、less、商品排序',
+            children: [
+              '/zhufengweb/prototype-THIS-shop_sort-regex/less/README.md',
+              '/zhufengweb/prototype-THIS-shop_sort-regex/prototype-THIS/README.md',
+              '/zhufengweb/prototype-THIS-shop_sort-regex/shop_sort/README.md',
+            ]
+          },
+          {
+            text: '变量提升、闭包、面向对象模型、this指向',
+            children: [
+              '/zhufengweb/variable_hoisting-closure-THIS-OOP/variable_hoisting-closure/README.md',
+              '/zhufengweb/variable_hoisting-closure-THIS-OOP/oop/README.md',
+              '/zhufengweb/variable_hoisting-closure-THIS-OOP/tool/README.md',
+              '/zhufengweb/variable_hoisting-closure-THIS-OOP/summary/README.md',
+            ]
+          },
+          {
+            text: 'webapp',
+            children: [
+              '/zhufengweb/webapp/README.md',
+            ]
+          },
+          {
+            text: 'node和promise',
+            children: [
+              '/zhufengweb/NODE-PROMISE/NODE-promise/README.md',
+              '/zhufengweb/NODE-PROMISE/promiseA+/README.md',
+              '/zhufengweb/NODE-PROMISE/storage/README.md',
+            ]
+          },
+          {
             text: '其他',
             children: [
               '/zhufengweb/AJAX/README.md',
@@ -76,10 +116,6 @@ module.exports = {
           },
 
         ]
-      },
-      {
-        text: 'GitHub',
-        link: 'https://github.com/zhao-yi-fan/note/',
       },
     ],
     sidebar: {
@@ -94,6 +130,24 @@ module.exports = {
             '/zhufengweb/DOM_Box_Model-JQ/DOM_Box_Model/README.md',
             '/zhufengweb/DOM_Box_Model-JQ/jquery/README.md',
             '/zhufengweb/DOM_Box_Model-JQ/waterfall/README.md',
+
+            '/zhufengweb/event_binding-publish_subscribe-event-event_delegation/event_binding-publish_subscribe/README.md',
+            '/zhufengweb/event_binding-publish_subscribe-event-event_delegation/event-event_delegation/README.md',
+
+            '/zhufengweb/prototype-THIS-shop_sort-regex/less/README.md',
+            '/zhufengweb/prototype-THIS-shop_sort-regex/prototype-THIS/README.md',
+            '/zhufengweb/prototype-THIS-shop_sort-regex/shop_sort/README.md',
+
+            '/zhufengweb/variable_hoisting-closure-THIS-OOP/variable_hoisting-closure/README.md',
+            '/zhufengweb/variable_hoisting-closure-THIS-OOP/oop/README.md',
+            '/zhufengweb/variable_hoisting-closure-THIS-OOP/tool/README.md',
+            '/zhufengweb/variable_hoisting-closure-THIS-OOP/summary/README.md',
+
+            '/zhufengweb/webapp/README.md',
+
+            '/zhufengweb/NODE-PROMISE/NODE-promise/README.md',
+            '/zhufengweb/NODE-PROMISE/promiseA+/README.md',
+            '/zhufengweb/NODE-PROMISE/storage/README.md',
           ]
         },
       ],
