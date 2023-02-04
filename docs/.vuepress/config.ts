1169560c159f275f3b3003fadcc697f901bccde0
赵一凡 <993445491@qq.com>
2023-02-04 22:56:39 +0800

    chore: test

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,11 @@ import recoTheme from 'vuepress-theme-reco'
 import themeConfig from './config/themeConfig'
 
 export default defineUserConfig({
-  title: "zhaoyifan Note",
-  description: 'zhaoyifan zhaoyifan Note',
+  title: "zhaoyifan Blog",
+  description: 'zhaoyifan zhaoyifan Blog',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
+  ],
   theme: recoTheme(themeConfig),
 })
