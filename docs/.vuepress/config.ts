1b201e769b0c4f175074d10e4cc26f4da2781993
zhaoyifan <993445491@qq.com>
2026-05-06 17:09:23 +0800

    chore: upgrade vuepress reco theme

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,4 +1,5 @@
 import { defineUserConfig } from "vuepress";
+import { viteBundler } from "@vuepress/bundler-vite";
 import recoTheme from 'vuepress-theme-reco'
 import themeConfig from './config/themeConfig'
 
@@ -10,6 +11,7 @@ export default defineUserConfig({
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
     ['meta', { name: 'baidu-site-verification', content: 'codeva-35wm7P5Tff' }],
   ],
+  bundler: viteBundler(),
   theme: recoTheme(themeConfig),
   lang: 'zh-CN',
 })
