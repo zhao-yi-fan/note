724dad6a0e31ed7b10e94fed6a3d61827be903e5
zhaoyifan <993445491@qq.com>
2026-07-22 11:23:54 +0800

    补充 promisify 前回调示例

--- playground/功能程序题/promise/promisify.js
+++ playground/功能程序题/promise/promisify.js
@@ -1,5 +1,14 @@
 const fs = require("fs");
 
+// 没有 promisify 时，需要直接使用回调函数处理异步结果
+/* fs.readFile(__filename, "utf8", (err, content) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(content.length);
+}); */
+
 // promisify 主要在 Node 环境中使用，用于转换 error-first callback 风格的 API
 function promisify(fn) {
   return function (...args) {
