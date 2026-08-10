8eb257089a54ef4365ca62d6be09a44462390178
zhaoyifan <993445491@qq.com>
2026-08-10 09:08:39 +0800

    修正 CommonJS 导出示例

--- playground/backend/nodejs/require module.exports exports关系 执行顺序/temp1.js
+++ playground/backend/nodejs/require module.exports exports关系 执行顺序/temp1.js
@@ -22,7 +22,7 @@ let fn1 = b => {
 let fn2 = b => {
     return a - b;
 };
-module.exports = {
+exports = {
     fn1: fn1
 };//=> 是无法导出内容的: 默认和module.exports是同一个堆内存, 但是这种操作让exports指向新的堆内存, 而module.exports不受影响(require导入的是module.exports对应的堆内存, 而不是exports的)
 /* module.exports = {//=> 重定向到自己的堆内存, 用来实现导出
@@ -30,5 +30,4 @@ module.exports = {
 } */
 exports.fn2 = fn2;//=> 向新的堆内存中加入导出内容
 exports.fn3 = 100;//=> 无法导出: 此时exports和module.exports已经不是同一个堆内存了
-
 
