8dbafb766e63e5f6b45ac8c66a50d99fa429f86d
zhaoyifan <993445491@qq.com>
2026-09-04 21:09:34 +0800

    chore: clean vuepress client styles

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,41 +1,12 @@
 import { defineClientConfig } from "@vuepress/client";
 import { onMounted } from "vue";
 import PlaygroundFrame from "./components/PlaygroundFrame.vue";
-// import ElementPlus from 'element-plus'
-// import 'element-plus/dist/index.css'
-
-// import hljs from 'highlight.js'
-// import 'highlight.js/styles/googlecode.css'
-
-// import BunnyUi from 'bunny-ui'
-// import 'bunny-ui/dist/bunnyUi.css'
-
-// Vue.directive('highlight',function (el){
-//   let blocks = el.querySelectorAll('pre code')
-//   blocks.forEach(block => {
-//     hljs.highlightBlock(block)
-
-//   });
-// })
-
-// export default ({
-//   Vue,
-//   options,
-//   router,
-//   siteData
-// }) => {
-//   Vue.use(Element)
-//   Vue.use(moonUi)
-// }
 
 export default defineClientConfig({
   enhance({ app }) {
     app.component("PlaygroundFrame", PlaygroundFrame);
-    // app.use(ElementPlus);
-    // app.use(BunnyUi)
   },
   setup() {
-    // 添加导航栏下拉菜单滚动功能
     onMounted(() => {
       if (typeof window !== "undefined") {
         // 只添加最大高度和滚动功能
@@ -50,5 +21,4 @@ export default defineClientConfig({
       }
     });
   },
-  rootComponents: [],
 });
